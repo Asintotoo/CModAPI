d89dc0b307f080255a6b0bf26dc05fbe25cf82da
Asintoto <166311378+Asintotoo@users.noreply.github.com>
2024-05-23 17:22:47 +0200

    Updated files to version 2.1-SNAPSHOT

---
+++ api/enums/CheckType.java
@@ -0,0 +1,15 @@
+
+package com.asintoto.cmod.api.enums;
+
+/**
+ *
+ * @author Asintoto
+ */
+public enum CheckType {
+    CHAT_A,
+    CHAT_B,
+    COMMAND_A,
+    COMMAND_B,
+    FLOOD_A,
+    TAB_A
+}
